5395617fb5abdffa533d67c70212273a5677817c
keltonhalbert <keltonhalbert@tempestchasing.com>
2014-05-16 16:30:16 -0500

    updated gui

--- sharppy/gui.py
+++ sharppy/gui.py
@@ -1,8 +1,9 @@
 import sys
 import numpy as np
 from PySide import QtGui, QtCore
-from sharppy.viz import plotSkewT, plotHodo, plotText
+from sharppy.viz import plotSkewT, plotHodo, plotText, plotAnalogues
 from sharppy.viz import plotThetae, plotWinds, plotSpeed, plotKinematics
+from sharppy.viz import plotSlinky, plotWatch, plotAdvection, plotSTP
 from sharppy.sounding import prof, prof2, prof3, plot_title
 from sharppy.sharptab import params
 
@@ -43,12 +44,12 @@ grid.addWidget(sound, 0, 0, 3, 1)
 # Handle the Upper Right
 urparent = QtGui.QFrame()
 urparent_grid = QtGui.QGridLayout()
-urparent_grid.setContentsMargins(0, 0, 0, sound.bpad-1)
+urparent_grid.setContentsMargins(0, 0, 0, 0)
 urparent.setLayout(urparent_grid)
 ur = QtGui.QFrame()
 ur.setStyleSheet("QFrame {"
                  "  background-color: rgb(0, 0, 0);"
-                 "  border-width: 1px;"
+                 "  border-width: 0px;"
                  "  border-style: solid;"
                  "  border-color: rgb(255, 255, 255);"
                  "  margin: 0px;}")
@@ -66,19 +67,19 @@ grid2.setContentsMargins(0, 0, 0, 0)
 ur.setLayout(grid2)
 speed_vs_height = plotSpeed( prof )
 speed_vs_height.setObjectName("svh")
-inferred_temp_advection = QtGui.QFrame()
+inferred_temp_advection = plotAdvection()
 hodo = plotHodo(prof.hght, prof.u, prof.v, prof=prof)
-storm_slinky = QtGui.QFrame()
+storm_slinky = plotSlinky()
 thetae_vs_pressure = plotThetae(prof)
 srwinds_vs_height = plotWinds(prof)
-watch_type = QtGui.QFrame()
-grid2.addWidget(speed_vs_height, 0, 0, 10, 3)
-grid2.addWidget(inferred_temp_advection, 0, 3, 10, 2)
+watch_type = plotWatch()
+grid2.addWidget(speed_vs_height, 0, 0, 11, 3)
+grid2.addWidget(inferred_temp_advection, 0, 3, 11, 2)
 grid2.addWidget(hodo, 0, 5, 8, 24)
-grid2.addWidget(storm_slinky, 8, 5, 2, 6)
-grid2.addWidget(thetae_vs_pressure, 8, 11, 2, 6)
-grid2.addWidget(srwinds_vs_height, 8, 17, 2, 6)
-grid2.addWidget(watch_type, 8, 23, 2, 6)
+grid2.addWidget(storm_slinky, 8, 5, 3, 6)
+grid2.addWidget(thetae_vs_pressure, 8, 11, 3, 6)
+grid2.addWidget(srwinds_vs_height, 8, 17, 3, 6)
+grid2.addWidget(watch_type, 8, 23, 3, 6)
 urparent_grid.addWidget(brand, 0, 0, 1, 0)
 urparent_grid.addWidget(ur, 1, 0, 50, 0)
 grid.addWidget(urparent, 0, 1, 3, 1)
@@ -97,12 +98,12 @@ convective = plotText(prof)
 grid3.setContentsMargins(0, 0, 0, 0)
 #kinematic = QtGui.QFrame()
 kinematic = plotKinematics(prof)
-misc1 = QtGui.QFrame()
-misc2 = QtGui.QFrame()
+SARS = plotAnalogues(prof)
+stp = plotSTP()
 grid3.addWidget(convective, 0, 0)
 grid3.addWidget(kinematic, 0, 1)
-grid3.addWidget(misc1, 0, 2)
-grid3.addWidget(misc2, 0, 3)
+grid3.addWidget(SARS, 0, 2)
+grid3.addWidget(stp, 0, 3)
 text.setLayout(grid3)
 grid.addWidget(text, 3, 0, 1, 2)
 pixmap = QtGui.QPixmap.grabWidget(mainWindow)
