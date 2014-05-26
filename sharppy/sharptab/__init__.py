cab6217aa774340f305bb31a1a712a6d7a275bf6
keltonhalbert <keltonhalbert@tempestchasing.com>
2014-05-26 14:06:55 -0500

    pushed temperature advection

--- sharppy/sharptab/__init__.py
+++ sharppy/sharptab/__init__.py
@@ -7,5 +7,5 @@ import winds
 import params
 import sars
 import watch_type
-
-__all__ = ['contants', 'utils', 'profile', 'params', 'thermo', 'interp', 'winds', 'sars', 'watch_type']
+import inferred_temp_adv
+__all__ = ['contants', 'utils', 'profile', 'params', 'thermo', 'interp', 'winds', 'sars', 'watch_type', 'inferred_temp_adv']
