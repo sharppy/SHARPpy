d3367fdd8403843f975840e98a9b0941d8103b92
keltonhalbert <keltonhalbert@tempestchasing.com>
2014-07-02 23:18:52 -0500

    added hex colors to constants

--- sharppy/sharptab/constants.py
+++ sharppy/sharptab/constants.py
@@ -1,6 +1,7 @@
 ''' Frequently used meteorological constants '''
 
-__all__ = ['MISSING', 'ROCP', 'ZEROCNK', 'G', 'TOL']
+__all__ = ['MISSING', 'ROCP', 'ZEROCNK', 'G', 'TOL', 'WHITE', 'RED',
+           'YELLOW', 'MAGENTA', 'DBROWN', 'LBROWN']
 
 # Meteorological Constants
 MISSING = -9999.0       # Missing Flag
@@ -8,3 +9,12 @@ ROCP = 0.28571426       # R over Cp
 ZEROCNK = 273.15        # Zero Celsius in Kelvins
 G = 9.80665             # Gravity
 TOL = 1e-10             # Floating Point Tolerance
+
+#colors
+WHITE = '#FFFFFF'
+RED = '#BF0000'
+YELLOW = '#FFFF00'
+MAGENTA = '#E700DF'
+DBROWN = '#775000'
+LBROWN = '#996600'
+
