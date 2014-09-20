957f65c323ce4f6be2bdca59eac309d24f15abac
keltonhalbert <keltonhalbert@tempestchasing.com>
2014-09-19 20:28:29 -0500

    moved around some things that were hardcoded as a temporary fix

--- sharppy/viz/skew.py
+++ sharppy/viz/skew.py
@@ -308,8 +308,6 @@ class plotSkewT(backgroundSkewT):
         self.tmp_stdev = prof.tmp_stdev
         self.u = prof.u; self.v = prof.v
         self.wetbulb = prof.wetbulb
-        self.dpcl_ttrace = prof.dpcl_ttrace
-        self.dpcl_ptrace = prof.dpcl_ptrace
         self.logp = np.log10(prof.pres)
         self.pcl = kwargs.get('pcl', None)
         self.proflist = kwargs.get('proflist', None)
@@ -438,6 +436,8 @@ class plotSkewT(backgroundSkewT):
         for h in [0,1000.,3000.,6000.,9000.,12000.,15000.]:
             self.draw_height(h, qp)
         if self.pcl is not None:
+            self.dpcl_ttrace = self.prof.dpcl_ttrace
+            self.dpcl_ptrace = self.prof.dpcl_ptrace
             self.drawVirtualParcelTrace(self.pcl.ttrace, self.pcl.ptrace, qp)
             self.drawVirtualParcelTrace(self.dpcl_ttrace, self.dpcl_ptrace, qp, color="#FF00FF")
         self.draw_parcel_levels(qp)
