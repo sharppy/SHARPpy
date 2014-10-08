d79d79b30d3053ae4286ba156d0e8150cf209490
Greg Blumberg <wblumberg@ou.edu>
2014-10-08 01:39:54 -0500

    Began adding SHARP precip type functionality

--- sharppy/sharptab/watch_type.py
+++ sharppy/sharptab/watch_type.py
@@ -11,7 +11,159 @@ def wind_chill(prof):
     wind_chill = 35.74 + (0.6215*sfc_temp) - (35.75*(sfc_wspd**0.16))\
                + 0.4275 * (sfc_temp) * (sfc_wspd**0.16)
     return wind_chill
+
+def init_phase(prof):
+    '''
+        Determine the initial phase of any precipitation source in the profile.
+
+        '''
+
+    plevel = 0
+    phase = -1
+
+    if prof.omega.all() is not ma.masked:
+        # No vertical velocity data to be used
+        below_5km_idx = np.ma.where(prof.hght[1:] < interp.to_msl(prof, 5000.))[0]
+    else:
+        # Use the VV to find the source of precip.
+        below_5km_idx = np.ma.where((prof.hght[1:] < interp.to_msl(prof, 5000.)) & (prof.omeg <= 0))[0]    
+   
+    # Compute the RH at the top and bottom of 50 mb layers
+    rh = thermo.relh(prof.pres[1:][below_5km_idx], prof.tmpc[1:][below_5km_idx], prof.dwpc[1:][below_5km_idx]):
+    new_pres = prof.pres[1:][below_5km_idx] + 50.
+    new_temp = interp.temp(prof, new_pres)
+    new_dwpt = interp.dwpt(prof, new_pres)
+    rh_plus50 = thermo.relh(new_pres, new_temp, new_dwpt):
     
+    # Find layers where the RH is >80% at the top and bottom
+    layers_idx = np.ma.where((rh_plus50 > 80) & (rh > 80))[0]
+
+    if len(layers_idx) == 0:
+        st = "N/A"
+        return plevel, phase, st
+
+    # Find the highest layer up via the largest index
+    top_most_layer = np.ma.max(layers_idx)
+    plevel = new_pres[top_most_layer] - 25.
+
+    # Determine the initial precip type based on the temp in the layer
+    tmp = interp.temp(prof, plevel)
+    if tmp > 0:
+        phase = 0
+        st = "Rain"
+    elif tmp <= 0 and tmp > -5:
+        phase = 1
+        st = "Freezing Rain"
+    elif tmp <=-5 and tmp > -9:
+        phase = 1
+        st = "ZR/S Mix"
+    elif tmp <= -9:
+        phase = 3
+        st = "Snow"
+    else:
+        st = "N/A"
+
+    return plevel, phase, st
+
+def posneg_temperature(prof, start):
+    return
+
+def posneg_wetbulb(prof, start):
+
+
+    # Find lowest obs in layer
+    lower = prof.pres[prof.get_sfc()]
+    lptr  = prof.get_sfc()
+
+    # Find the highest obs in the layer
+    if start == -1:
+        lvl, phase, st = init_phase(prof)
+        if lvl > 0:
+            upper = lvl
+        else:
+            upper = 500.
+    else:
+        upper = start
+
+    # Something needs to be done here to get the right uptr
+    # I don't want to duplicate this loop
+    '''
+        i=numlvl-1;
+        while(sndg[i][pIndex] < upper) {
+            i--;
+            if (i < 0) {
+                fprintf(stderr,
+                 "Warning: posneg_temp: Could not find a pressure greater than %.2f\n",
+                   upper);
+                fprintf(stderr, "Using %.2f as the upper level.\n",
+                  sndg[0][pIndex]);
+                i = 0;
+                break;
+                }
+        }
+        uptr = i;
+        if (sndg[i][pIndex] == upper)
+            uptr--;
+    '''
+    # Start with the upper layer
+    pe1 = upper;
+    h1 =  interp.hght(prof, pe1);
+    te1 = wetbulb(pe1, interp.temp(prof, pe1), interp.dwpt(prof, pe1))
+    tp1 = 0
+
+    totp = totn = tote = ptop = pbot = lyrlast = 0
+
+    for i in np.arange(uptr, lptr-1, -1):
+        pe2 = prof.pres[i]
+        h2 = prof.hght[i]
+        te2 = thermo.wetbulb(pe2, interp.temp(prof, pe2), interp.dwpt(prof, pe2))
+        tp2 = 0
+        tdef1 = (0 - te1) / (te1 + 273.15);
+        tdef2 = (0 - te2) / (te2 + 273.15);
+        lyrlast = lyre;
+        lyre = 9.8 * (tdef1 + tdef2) / 2.0 * (h2 - h1);
+
+        # Has a warm layer been found yet?
+        if te2 > 0:
+            if warmlayer == 0:
+                warmlayer = 1
+                ptop = pe2
+
+        # Has a cold layer been found yet?
+        if te2 < 0:
+            if warmlayer == 1 and coldlayer == 0:
+                coldlayer = 1
+                pbot = pe2
+
+        if warmlayer > 0:
+            if lyre > 0:
+                totp += lyre
+            else:
+                totn += lyre
+            tote += lyre
+
+        pelast = pe1
+        pe1 = pe2
+        h1 = h2
+        te1 = te2
+        tp1 = tp2
+    
+    if warmlayer == 1 and coldlayer == 1:
+        pos = totp
+        neg = totn
+        top = ptop
+        bot = pbot
+    else:
+        neg = 0
+        pos = 0
+        bot = 0
+        top = 0
+
+    return pos, neg, top, bot
+
+def best_guess_precip(struct):
+    return
+
 def precip_type(prof):
     #
     # This function looks at the current SHARPPY profile (prof)
