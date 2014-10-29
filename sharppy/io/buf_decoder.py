ebab18213360052bbaf5155e3e969e0320abb0a9
keltonhalbert <keltonhalbert@tempestchasing.com>
2014-10-29 16:04:49 -0500

    f.close() causes the program to hang for a substantial amount of time... commented out.

--- sharppy/io/buf_decoder.py
+++ sharppy/io/buf_decoder.py
@@ -55,7 +55,9 @@ class BufkitFile(object):
         self.dates = []
         self.dates_loaded = False
         self.member_names = []
+        print "Start2"
         self.__readFile()
+        print "End2"
         self.num_profiles = 0
  
     def parseBUF(self, text):
@@ -162,7 +164,6 @@ class BufkitFile(object):
             self.omeg.append(omeg)
             self.numProfiles = numProfiles
             self.profile_length = profile_length
-
         self.hght = np.asarray(self.hght)
         self.pres = np.asarray(self.pres)
         self.tmpc = np.asarray(self.tmpc)
@@ -170,7 +171,7 @@ class BufkitFile(object):
         self.wdir = np.asarray(self.wdir)
         self.wspd = np.asarray(self.wspd)
         self.omeg = np.asarray(self.omeg)
-        f.close()
+        #f.close()
 
     def getNumMembers(self):
         return self.num_members
