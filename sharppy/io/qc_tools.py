d8cf560a49d7def63eac9570145270c1d5535687
Greg Blumberg <wblumberg@ou.edu>
2014-11-08 20:58:25 -0600

    Added documentation to qc_tools.

--- sharppy/io/qc_tools.py
+++ sharppy/io/qc_tools.py
@@ -5,17 +5,78 @@ __all__ = ['raiseError', 'numMasked', 'isPRESValid', 'isHGHTValid', 'isWSPDValid
 __all__ += ['isDWPCValid', 'isTMPCValid']
 
 def raiseError(string, errorType):
+    '''
+        raiseError
+
+        This function will raise an exception specified by the errorType variable.
+
+        Parameters
+        ----------
+        string: the string to be printed out to the user.
+        errorType: the type of exception.
+
+        Returns
+        -------
+        None
+    '''
     raise errorType, string
 
 def numMasked(arr):
+    '''
+        numMasked
+
+        This function returns the number of unmasked array elements as well as the
+        length of the array.
+
+        Parameters
+        ----------
+        arr: a masked Numpy array
+
+        Returns
+        -------
+        the number of unmasked array elements
+        the length of the unmasked array
+    '''
     return arr.count(), arr.shape[0]
 
 def areProfileArrayLengthEqual(prof):
+    '''
+        areProfileArrayLengthEqual
+
+        This function checks to see if all of the arrays passed to a Profile object
+        have the same length.  If they don't have the same length, then this function
+        raises an AssertionError exception.
+
+        Parameters
+        ----------
+        prof: a Profile object
+
+        Returns
+        -------
+        None
+    '''
+
     if not (len(prof.pres) == len(prof.hght) == len(prof.tmpc) == len(prof.dwpc) ==\
             len(prof.wdir) == len(prof.wspd) == len(prof.u) == len(prof.v) == len(prof.omeg)):
         raiseError("Arrays passed to the Profile object have unequal lengths.", AssertionError)
  
 def isPRESValid(pres):
+    '''
+        isPRESValid
+
+        This function checks to see if valid pressure (mb)
+        values are within the pres array that is passed to the 
+        Profile object
+
+        Parameters
+        ----------
+        pres: the pressure array (mb)
+
+        Returns
+        -------
+        True/False: True if the pressure array length is > 0 and
+                    if the pressure array is decreasing as the indices increase.
+    '''
     idx = np.ma.argsort(pres)[::-1]
     idx2 = np.ma.where(pres <= 0)[0]
     num_ok, total = numMasked(pres)
@@ -26,20 +87,20 @@ def isPRESValid(pres):
 
 def isHGHTValid(hght):
     '''
-        isWDIRValid
+        isHGHTValid
 
-        This function checks to see if valid wind direction (deg)
-        values are within the wdir array that is passed to the 
+        This function checks to see if valid height (m)
+        values are within the hght array that is passed to the 
         Profile object
 
         Parameters
         ----------
-        wdir: the wind direction array (degrees)
+        hght: the height array (m)
 
         Returns
         -------
-        True/False: True if the wind direction array is > 0 in size and
-                    if the wind directions are between 0 and 360.
+        True/False: True if the height array length is > 0 and
+                    if the height array is increasing with the index.
     '''
     idx = np.ma.argsort(hght)
     idx2 = np.ma.where(hght < 0)[0]
