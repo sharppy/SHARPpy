4dcff27ce73fedbc058dcbd12a01a060e92fee78
keltonhalbert <keltonhalbert@tempestchasing.com>
2014-12-24 19:32:45 -0600

    Reverting away from Cython. Not enough performance.

--- sharppy/sharptab/thermo.py
+++ sharppy/sharptab/thermo.py
@@ -232,9 +232,9 @@ def wobf(t):
     else:
         if t is np.ma.masked:
             return t
-        t = t + 20.
-        return cthermo.wobf(t)
-        """if t <= 0:
+        #t = t + 20.
+        #return cthermo.wobf(t)
+        if t <= 0:
             npol = 1. + t * (-8.841660499999999e-3 + t * ( 1.4714143e-4 + t * (-9.671989000000001e-7 + t * (-3.2607217e-8 + t * (-3.8598073e-10)))))
             npol = 15.13 / (np.power(npol,4))
             return npol
@@ -242,7 +242,7 @@ def wobf(t):
             ppol = t * (4.9618922e-07 + t * (-6.1059365e-09 + t * (3.9401551e-11 + t * (-1.2588129e-13 + t * (1.6688280e-16)))))
             ppol = 1 + t * (3.6182989e-03 + t * (-1.3603273e-05 + ppol))
             ppol = (29.93 / np.power(ppol,4)) + (0.96 * t) - 14.8
-            return ppol"""
+            return ppol
 
 
 
@@ -263,28 +263,26 @@ def satlift(p, thetam):
     Temperature (C) of saturated parcel at new level
 
     '''
-    if type(p) == type(np.array([p])) or type(thetam) == type(np.array([thetam])):
-        if np.fabs(p - 1000.) - 0.001 <= 0: return thetam
-        eor = 999
-        while np.fabs(eor) - 0.1 > 0:
-            if eor == 999:                  # First Pass
-                pwrp = np.power((p / 1000.),ROCP)
-                t1 = (thetam + ZEROCNK) * pwrp - ZEROCNK
-                e1 = wobf(t1) - wobf(thetam)
-                rate = 1
-            else:                           # Successive Passes
-                rate = (t2 - t1) / (e2 - e1)
-                t1 = t2
-                e1 = e2
-            t2 = t1 - (e1 * rate)
-            e2 = (t2 + ZEROCNK) / pwrp - ZEROCNK
-            e2 += wobf(t2) - wobf(e2) - thetam
-            eor = e2 * rate
-        return t2 - eor
-    elif p is np.ma.masked or thetam is np.ma.masked:
+    #if type(p) == type(np.array([p])) or type(thetam) == type(np.array([thetam])):
+    if p is np.ma.masked or thetam is np.ma.masked:
         return np.ma.masked
-    else:
-        return cthermo.satlift(p, thetam)
+    if np.fabs(p - 1000.) - 0.001 <= 0: return thetam
+    eor = 999
+    while np.fabs(eor) - 0.1 > 0:
+        if eor == 999:                  # First Pass
+            pwrp = np.power((p / 1000.),ROCP)
+            t1 = (thetam + ZEROCNK) * pwrp - ZEROCNK
+            e1 = wobf(t1) - wobf(thetam)
+            rate = 1
+        else:                           # Successive Passes
+            rate = (t2 - t1) / (e2 - e1)
+            t1 = t2
+            e1 = e2
+        t2 = t1 - (e1 * rate)
+        e2 = (t2 + ZEROCNK) / pwrp - ZEROCNK
+        e2 += wobf(t2) - wobf(e2) - thetam
+        eor = e2 * rate
+    return t2 - eor
 
 
 def wetlift(p, t, p2):
@@ -306,15 +304,13 @@ def wetlift(p, t, p2):
 
     '''
     thta = theta(p, t, 1000.)
-    if type(thta) == type(np.array([thta])):
-        c1 = wobf(thta)
-        c2 = wobf(t)
-        thetam = thta - c1 + c2
-        return satlift(p2, thetam)
-    elif thta is np.ma.masked or p2 is np.ma.masked:
+    if thta is np.ma.masked or p2 is np.ma.masked:
         return np.ma.masked
-    else:
-        return cthermo.wetlift(thta, t, p2)
+    c1 = wobf(thta)
+    c2 = wobf(t)
+    thetam = thta - c1 + c2
+    return satlift(p2, thetam)
+
 
 
 def lifted(p, t, td, lev):
