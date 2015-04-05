d95cba06efeb9ac1cd2ee9e8d51b8b94cead9d95
keltonhalbert <keltonhalbert@tempestchasing.com>
2015-04-05 00:07:31 -0500

    Added to imports

--- sharppy/__init__.py
+++ sharppy/__init__.py
@@ -1,8 +1,10 @@
 import version
 import sharptab
 import viz
+import io
 import databases
+import tests
 
-__all__ = ['version', 'sharptab', 'viz', 'databases']
+__all__ = ['version', 'sharptab', 'viz', 'databases', 'io', 'tests']
 
 __version__ = version.get_version()
