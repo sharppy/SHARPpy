380e7d6eb61163dac7e52e00827a5a89342086af
Tim Supinie <tsupinie@ou.edu>
2015-04-06 21:38:44 -0500

    Cleaned up repository (deleted some files that aren't being used) and fixed a bug with __init__.py

--- sharppy/__init__.py
+++ sharppy/__init__.py
@@ -3,8 +3,7 @@ import sharptab
 import viz
 import io
 import databases
-import tests
 
-__all__ = ['version', 'sharptab', 'viz', 'databases', 'io', 'tests']
+__all__ = ['version', 'sharptab', 'viz', 'databases', 'io']
 
 __version__ = version.get_version()
