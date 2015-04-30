ba4add104c29f0c42f2068bb97e8e8a812998128
keltonhalbert <keltonhalbert@tempestchasing.com>
2015-04-29 20:23:41 -0500

    setup now copies over from downloaded directory into the .sharppy directory

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, shutil, glob
 from setuptools import setup, find_packages
 
 
@@ -24,14 +24,33 @@ long_description = ""
 license = "BSD"
 keywords = "meteorology soundings analysis"
 url = "https://github.com/sharppy/SHARPpy"
-packages = find_packages()
+packages = ['sharppy', 'sharppy.databases', 'sharppy.io', 'sharppy.sharptab', 'sharppy.viz']
 package_data = {"": ["*.md", "*.txt", "*.png", "databases/sars/hail/*", "databases/sars/supercell/*",
                      "databases/shapefiles/*"],}
 include_package_data = True
 classifiers = ["Development Status :: 4 - Beta"]
 
-if not os.path.exists(os.path.join(os.path.expanduser("~"), ".sharppy")):
-    os.makedirs(os.path.join(os.path.expanduser("~"), ".sharppy"))
+HOME_PATH = os.path.join(os.path.expanduser("~"), ".sharppy")
+
+## if the settings directory does not exist, create it and copy the necessary resources
+if not os.path.exists(HOME_PATH):
+    os.makedirs(HOME_PATH)
+    shutil.copytree(os.path.join(os.path.dirname(__file__), "datasources"), os.path.join(HOME_PATH, "datasources"))
+
+## if a datasource XML file exits, we don't want to overwrite it, so copy it to a different file first
+if os.path.exists(os.path.join(HOME_PATH, "datasources", "standard.xml")):
+    shutil.copy(os.path.join(HOME_PATH, "datasources", "standard.xml"),
+                os.path.join(HOME_PATH, "datasources", "standard.xml.old"))
+
+    ## copy over the new file
+    shutil.copy(os.path.join(os.path.dirname(__file__), "datasources", "standard.xml"),
+                os.path.join(HOME_PATH, "datasources", "standard.xml"))
+
+    ## copy over the standard CSV files
+    CSVs = glob.glob(os.path.join(os.path.dirname(__file__), "datasources", "*.csv"))
+    for csv in CSVs:
+        shutil.copy(csv, os.path.join(HOME_PATH, "datasources", os.path.basename(csv)))
+
 
 setup(
     name = name,
