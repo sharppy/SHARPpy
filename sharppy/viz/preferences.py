e2b8199d9bc6c0bcf526da3a46afbb76c25b2116
Greg Blumberg <wblumberg@ou.edu>
2016-09-03 00:36:33 -0500

    fixed bug associated with PWAT unit preferences.

--- sharppy/viz/preferences.py
+++ sharppy/viz/preferences.py
@@ -244,7 +244,7 @@ class PrefDialog(QDialog):
         layout.addWidget(wind_units_box)
         
         pw_units_box, self.pw_units = PrefDialog._createRadioSet("Precipitable Water Vapor Units", ["in", "cm"], default=self._config['preferences', 'pw_units'])
-        layout.addWidget(pw_vector_box)
+        layout.addWidget(pw_units_box)
         
         calc_vector_box, self.calc_vector = PrefDialog._createRadioSet("Storm Motion Vector Used in Calculations", ["Left Mover", "Right Mover"], default=self._config['preferences', 'calc_vector'])
         layout.addWidget(calc_vector_box)
