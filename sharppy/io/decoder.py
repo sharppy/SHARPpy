3aa1758cc951e0113e71dad7fda2877974c8b823
tsupinie <tsupinie@gmail.com>
2017-03-30 20:11:54 -0500

    Switched to opening disk files in binary mode (fixes bugs on Windows).

--- sharppy/io/decoder.py
+++ sharppy/io/decoder.py
@@ -76,7 +76,7 @@ class Decoder(object):
             f = urllib2.urlopen(self._file_name)
         except (ValueError, IOError):
             try:
-                f = open(self._file_name, 'r')
+                f = open(self._file_name, 'rb')
             except IOError:
                 raise IOError("File '%s' cannot be found" % self._file_name)
         file_data = f.read()
