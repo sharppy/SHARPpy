55e739cd828b2a643ed7023cd7237d3d2c7dde49
Greg Blumberg <wblumberg@ou.edu>
2018-11-20 22:33:31 -0500

    Swapped Sphinx to use napoleon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'numpydoc',
+    'sphinxcontrib.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
