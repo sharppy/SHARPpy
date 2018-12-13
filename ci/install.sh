6c4d897a23413ae485b5b4dcfc367332e7a5280f
Greg Blumberg <wblumberg@ou.edu>
2018-12-13 00:29:41 -0500

    Fixed install script

--- ci/install.sh
+++ ci/install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 echo $PYTHON_VERSION
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then if [[ "$PYTHON_VERSION" == "2.7" ]]; then
-curl https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -o miniconda.sh;
-else curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
--o miniconda.sh; fi else if [[ "$PYTHON_VERSION" == "2.7" ]]; then wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
--O miniconda.sh; else wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
--O miniconda.sh; fi fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
+    if [[ "$PYTHON_VERSION" == "2.7" ]]; then
+        curl https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -o miniconda.sh;
+    else
+        curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o miniconda.sh;
+    fi 
+  else 
+    if [[ "$PYTHON_VERSION" == "2.7" ]]; then 
+        wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh; 
+    else 
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh; 
+  fi
+fi
 
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
--- ci/make_binary.sh
+++ ci/make_binary.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Build the binaries
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then pyinstaller runsharp/SHARPpy-osx.spec
---onefile --noconsole; else pyinstaller runsharp/SHARPpy-linux-redhat5.spec; fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+     pyinstaller runsharp/SHARPpy-osx.spec --onefile --noconsole; 
+else 
+     pyinstaller runsharp/SHARPpy-linux-redhat5.spec;
+fi
 
 ls -lht dist/
 
