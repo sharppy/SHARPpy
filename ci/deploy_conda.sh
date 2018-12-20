e6efebf7013cf5ab9831cd4a1a064af5c4486444
Greg Blumberg <wblumberg@ou.edu>
2018-12-19 20:19:32 -0500

    Updated travis ci to only run tests on linux

--- ci/deploy_conda.sh
+++ ci/deploy_conda.sh
@@ -2,7 +2,8 @@
 # Inspired by: https://gist.github.com/zshaheen/fe76d1507839ed6fbfbccef6b9c13ed9
 
 # Download conda-build
-conda install -q conda-build anaconda
+echo "Download conda-build and anaconda-client"
+conda install -q conda-build anaconda-client
 
 # Set environmental variables 
 PKG_NAME=sharppy
@@ -10,19 +11,23 @@ USER=sharppy
 OS=$TRAVIS_OS_NAME-64
 
 # Make the build output directory
+echo "Make the build output directory"
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 
 # Set the build path and the current version
 export CONDA_BLD_PATH=~/conda-bld
-export VERSION=`date +%Y.%m.%d`
 
+cd ..
 # Build the conda recipe
-conda build ../conda_recipe/
+echo "Build the conda recipe"
+conda build conda_recipe/
 
 # Convert the conda package to support other operating systems
-conda convert $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-o.tar.bz2 - p all
+echo "Convert the recipe to other OSes"
+conda convert $CONDA_BLD_PATH/*/*.tar.bz2 --platform all --output_dir $CONDA_BLD_PATH
 
 # Upload to the conda package manager
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l release --force $CONDA_BLD_PATH/*/*.tar.bz2
 
+rm -rf ~/conda-bld
