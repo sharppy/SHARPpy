1131fcd452d1ec69c4677c7bc4d051ed94bc5123
Greg Blumberg <wblumberg@ou.edu>
2018-12-20 20:00:13 -0500

    Fix as travisCI couldnt find RTD-theme or numpy

--- ci/install.sh
+++ ci/install.sh
@@ -2,8 +2,7 @@
 echo $PYTHON_VERSION
 
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh; 
-chmod +x miniconda.sh
-./miniconda.sh -b
+bash miniconda.sh -b
 export PATH=/home/travis/miniconda3/bin:$PATH
 conda config --set always_yes yes
 conda config --set show_channel_urls true
