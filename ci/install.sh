048f05cb211e3d616ba30df2cf408767088e8029
Greg Blumberg <wblumberg@ou.edu>
2018-12-20 20:50:43 -0500

    Trying to repair TravisCI

--- ci/install.sh
+++ ci/install.sh
@@ -7,22 +7,19 @@ else
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh; 
 fi
 
-chmod +x miniconda.sh
-bash miniconda.sh -b
-export PATH=/home/travis/miniconda3/bin:$PATH
-conda config --set always_yes yes
-conda config --set show_channel_urls true
-conda update -q conda
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
 
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda install -n -q root _license
+
+conda config --add channels conda-forge
 conda env create -f ci/environment-$PYTHON_VERSION.yml
 source activate testenv
 
 pip install --upgrade pip
-
-if [[ "$BUILD_CONDA" == "YES" ]]; then
-    conda install -q conda-build
-    conda install -q jinja2 setuptools
-fi
 
 # If we're building on OSX, we need to download python.app to get around the qt_menu.nib problem.
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
