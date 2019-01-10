33e44e997b00c3d3acb22d8412da58d475ebaef7
Greg Blumberg <wblumberg@ou.edu>
2019-01-10 00:06:27 -0500

    Fixed syntax error in full_gui

--- runsharp/full_gui.py
+++ runsharp/full_gui.py
@@ -73,11 +73,6 @@ logging.info('PySide version: ' + str(PySide.__version__))
 logging.info("Python version: " + str(platform.python_version()))
 logging.info("Qt version: " + str(PySide.QtCore.__version__))
 
-if latest[0] is False:
-    logging.info("A newer release of SHARPpy was found on Github Releases.")
-else:
-    logging.info("This is the most recent version of SHARPpy.")
-
 # from sharppy._version import __version__#, __version_name__
 
 __version_name__ = ''
@@ -1096,6 +1091,11 @@ def main():
 
     # Check to see if there's a newer version of SHARPpy on Github Releases
     latest = check_latest()
+
+    if latest[0] is False:
+        logging.info("A newer release of SHARPpy was found on Github Releases.")
+    else:
+        logging.info("This is the most recent version of SHARPpy.")
 
     # Alert the user that there's a newer version on Github (and by extension through CI also on pip and conda)
     if latest[0] is False:
