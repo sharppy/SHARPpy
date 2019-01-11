1e6fc7d0a39e49c301a7d00310563f5c0d85b7b8
Greg Blumberg <wblumberg@ou.edu>
2019-01-11 14:35:48 -0500

    Added tests for ProfCollection in test_decoders

--- sharppy/tests/test_decoders.py
+++ sharppy/tests/test_decoders.py
@@ -18,20 +18,54 @@ def test_decoder():
     assert len(decoders) > 0
     
     dec = spc_decoder.SPCDecoder(files[0])
+    profs = dec.getProfiles()
+    profs._backgroundCopy("")
+    
+    # Test Interpolation
+    profs.interp()
+    assert profs.isInterpolated() == True
+    profs.resetInterpolation()
+    assert profs.isInterpolated() == False
+
+    # Test setting storm motion vectors 
+    profs.modifyStormMotion('right', 0, 0)
+    profs.modifyStormMotion('left', 0, 0)
+    profs.resetStormMotion()
+    
+    # Try modify
+    profs.modify(0, tmpc=35)
+    profs.modify(0, u=0)
+    tmp = profs._profs[""][0].tmpc
+    tmp[0:2] = 35
+    profs.modify(-999, tmpc=tmp, idx_range=[0,1])
+    profs.resetModification('tmpc')
+    profs.resetModification('u')
+    
+    # Load in a BUFKIT file     
     dec = buf_decoder.BufDecoder(files[1])
     profs = dec.getProfiles()
     stn_id = dec.getStnId()
 
+    # Test some of the characteristics of the ProfCollection
     assert profs.isEnsemble() == False
-    assert profs.isInterpolated() == False
     assert profs.isModified() == False
     assert profs.getAnalogDate() is None
     assert profs.hasCurrentProf() == True
    
+    # Try to load in the UWYO file
     dec = uwyo_decoder.UWYODecoder(files[2]) 
+
+    # Load in the PECAN-type files
     dec = pecan_decoder.PECANDecoder(files[3])
     dec = pecan_decoder.PECANDecoder(files[4])
-    assert dec.getProfiles().isEnsemble() == True
 
+    # Test some of the characteristics of this ProfCollection
+    assert dec.getProfiles().isEnsemble() == True
+    profs = dec.getProfiles()
+    profs.advanceHighlight(1)
+    profs.advanceHighlight(-1)
+    profs.advanceTime(1)
+    profs.advanceTime(-1)
     #print(profs) 
-test_decoder()
+
+#test_decoder()
