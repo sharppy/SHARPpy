91c9284c44a1906928ec87c0138e73f456b21d6e
Greg Blumberg <wblumberg@ou.edu>
2019-01-11 18:53:42 -0500

    Added dateutil to conda install on CI

--- ci/install.sh
+++ ci/install.sh
@@ -23,7 +23,7 @@ conda update -q conda
 conda install -n -q root _license
 conda info -a
 conda config --add channels conda-forge 
-conda create -q -n test-environment python=$PYTHON_VERSION pyside pyinstaller requests conda-build anaconda-client numpy=$NUMPY_VERSION
+conda create -q -n test-environment python=$PYTHON_VERSION pyside pyinstaller requests dateutil conda-build anaconda-client numpy=$NUMPY_VERSION
 source activate test-environment
 
 pip install --upgrade pip
