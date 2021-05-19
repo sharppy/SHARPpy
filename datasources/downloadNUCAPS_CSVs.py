9a3464b967caf53bd71ff46425c6e72a66d15189
Jeff Szkodzinski <jeff.szkodzinski@gmail.com>
2021-05-19 11:05:11 -0400

    activated all NUCAPS data for CONUS

--- datasources/downloadNUCAPS_CSVs.py
+++ datasources/downloadNUCAPS_CSVs.py
@@ -37,57 +37,57 @@ def downloadNewCSVs():
         with open('npp_conus.csv', 'wb') as f:
             shutil.copyfileobj(r, f)
 
-    # # Download Alaska, Caribbean and CONUS CSVs for Aqua.
+    # Download Alaska, Caribbean and CONUS CSVs for Aqua.
     # with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/alaska/sharppy/aq0/csv/aq0_alaska.csv')) as r:
     #     with open('aq0_alaska.csv', 'wb') as f:
     #         shutil.copyfileobj(r, f)
-    #
+
     # with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/caribbean/sharppy/aq0/csv/aq0_caribbean.csv')) as r:
     #     with open('aq0_caribbean.csv', 'wb') as f:
     #         shutil.copyfileobj(r, f)
-    #
-    # with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/conus/sharppy/aq0/csv/aq0_conus.csv')) as r:
-    #     with open('aq0_conus.csv', 'wb') as f:
-    #         shutil.copyfileobj(r, f)
 
-    # # Download Alaska, Caribbean and CONUS CSVs for Metop-A.
+    with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/conus/sharppy/aq0/csv/aq0_conus.csv')) as r:
+        with open('aq0_conus.csv', 'wb') as f:
+            shutil.copyfileobj(r, f)
+
+    # Download Alaska, Caribbean and CONUS CSVs for Metop-A.
     # with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/alaska/sharppy/m01/csv/m01_alaska.csv')) as r:
     #     with open('m01_alaska.csv', 'wb') as f:
     #         shutil.copyfileobj(r, f)
-    #
+
     # with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/caribbean/sharppy/m01/csv/m01_caribbean.csv')) as r:
     #     with open('m01_caribbean.csv', 'wb') as f:
     #         shutil.copyfileobj(r, f)
-    #
-    # with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/conus/sharppy/m01/csv/m01_conus.csv')) as r:
-    #     with open('m01_conus.csv', 'wb') as f:
-    #         shutil.copyfileobj(r, f)
-    #
-    # # Download Alaska, Caribbean and CONUS CSVs for Metop-B.
+
+    with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/conus/sharppy/m01/csv/m01_conus.csv')) as r:
+        with open('m01_conus.csv', 'wb') as f:
+            shutil.copyfileobj(r, f)
+
+    # Download Alaska, Caribbean and CONUS CSVs for Metop-B.
     # with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/alaska/sharppy/m02/csv/m02_alaska.csv')) as r:
     #     with open('m02_alaska.csv', 'wb') as f:
     #         shutil.copyfileobj(r, f)
-    #
+
     # with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/caribbean/sharppy/m02/csv/m02_caribbean.csv')) as r:
     #     with open('m02_caribbean.csv', 'wb') as f:
     #         shutil.copyfileobj(r, f)
-    #
-    # with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/conus/sharppy/m02/csv/m02_conus.csv')) as r:
-    #     with open('m02_conus.csv', 'wb') as f:
-    #         shutil.copyfileobj(r, f)
-    #
-    # # Download Alaska, Caribbean and CONUS CSVs for Metop-C.
+
+    with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/conus/sharppy/m02/csv/m02_conus.csv')) as r:
+        with open('m02_conus.csv', 'wb') as f:
+            shutil.copyfileobj(r, f)
+
+    # Download Alaska, Caribbean and CONUS CSVs for Metop-C.
     # with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/alaska/sharppy/m03/csv/m03_alaska.csv')) as r:
     #     with open('m03_alaska.csv', 'wb') as f:
     #         shutil.copyfileobj(r, f)
-    #
+
     # with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/caribbean/sharppy/m03/csv/m03_caribbean.csv')) as r:
     #     with open('m03_caribbean.csv', 'wb') as f:
     #         shutil.copyfileobj(r, f)
-    #
-    # with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/conus/sharppy/m03/csv/m03_conus.csv')) as r:
-    #     with open('m03_conus.csv', 'wb') as f:
-    #         shutil.copyfileobj(r, f)
+
+    with closing(request.urlopen(f'https://geo.nsstc.nasa.gov/SPoRT/jpss-pg/nucaps/gridded/conus/sharppy/m03/csv/m03_conus.csv')) as r:
+        with open('m03_conus.csv', 'wb') as f:
+            shutil.copyfileobj(r, f)
 
 def removeOldCSVs():
     # Remove old CSVs from HIDDEN_DSDIR before moving new CSVs to that directory.
@@ -140,18 +140,18 @@ def moveNewCSVs():
     for nppcsv in nppCSVs:
         shutil.move(nppcsv, HIDDEN_DSDIR)
 
-    # aq0CSVs = glob.glob(os.path.join(SHARPPY_DIR, "aq0*.csv"))
-    # for aq0csv in aq0CSVs:
-    #     shutil.move(aq0csv, HIDDEN_DSDIR)
+    aq0CSVs = glob.glob(os.path.join(SHARPPY_DIR, "aq0*.csv"))
+    for aq0csv in aq0CSVs:
+        shutil.move(aq0csv, HIDDEN_DSDIR)
 
-    # m01CSVs = glob.glob(os.path.join(SHARPPY_DIR, "m01*.csv"))
-    # for m01csv in m01CSVs:
-    #     shutil.move(m01csv, HIDDEN_DSDIR)
-    #
-    # m02CSVs = glob.glob(os.path.join(SHARPPY_DIR, "m02*.csv"))
-    # for m02csv in m02CSVs:
-    #     shutil.move(m02csv, HIDDEN_DSDIR)
-    #
-    # m03CSVs = glob.glob(os.path.join(SHARPPY_DIR, "m03*.csv"))
-    # for m03csv in m03CSVs:
-    #     shutil.move(m03csv, HIDDEN_DSDIR)
+    m01CSVs = glob.glob(os.path.join(SHARPPY_DIR, "m01*.csv"))
+    for m01csv in m01CSVs:
+        shutil.move(m01csv, HIDDEN_DSDIR)
+
+    m02CSVs = glob.glob(os.path.join(SHARPPY_DIR, "m02*.csv"))
+    for m02csv in m02CSVs:
+        shutil.move(m02csv, HIDDEN_DSDIR)
+
+    m03CSVs = glob.glob(os.path.join(SHARPPY_DIR, "m03*.csv"))
+    for m03csv in m03CSVs:
+        shutil.move(m03csv, HIDDEN_DSDIR)
